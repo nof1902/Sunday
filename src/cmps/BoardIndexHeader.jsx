5be7af3c5726cc2e17adb1e37877f1ad417826d9
nof1902 <nof1902@gmail.com>
2024-01-17 21:15:03 +0200

    add style "add board & group" buttons

--- src/cmps/BoardIndexHeader.jsx
+++ src/cmps/BoardIndexHeader.jsx
@@ -21,7 +21,7 @@ export function BoardIndexHeader() {
                 <section className='addition-actions'>
                     <button className='add-board'>New Project</button>
                     <button className='add-group'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down"><path d="m6 9 6 6 6-6"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-down"><path d="m6 9 6 6 6-6"/></svg>
                     </button>
                 </section>
             </section>
