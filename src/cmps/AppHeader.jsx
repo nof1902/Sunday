04ef26dca4410d9a9b3dab47db827656b3d7f331
OrenMela <120256051+OrenMela@users.noreply.github.com>
2024-01-20 15:57:15 +0200

    שינויים ב homepage

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -35,22 +35,22 @@ export function AppHeader() {
 
     return (
         <header className="app-header">
-                {/* {user &&
-                    <span className="user-info">
-                        <Link to={`user/${user._id}`}>
-                            {user.imgUrl && <img src={user.imgUrl} />}
-                            {user.fullname}
-                        </Link>
-                        <span className="score">{user.score?.toLocaleString()}</span>
-                        <button onClick={onLogout}>Logout</button>
-                    </span>
-                }
-                {!user &&
-                    <section className="user-info">
-                        <LoginSignup onLogin={onLogin} onSignup={onSignup} />
-                    </section>
-                } */}
-            <h1>My App</h1>
+            <section className='app-header-contents'>
+                <div className='app-header-contents-logo'>
+                    <img className='app-header-contents-logo-img' src="https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/remote_mondaycom_static/img/monday-logo-x2.png" alt="monday.com logo" />
+                </div>
+                <div className='app-header-contents-nav'>
+                    <div className='app-header-contents-nav-items'>Products</div>
+                    <div className='app-header-contents-nav-items'>Teams</div>
+                    <div className='app-header-contents-nav-items'>Platform</div>
+                    <div className='app-header-contents-nav-items'>Resources</div>
+                    <div className='app-header-nav-empty'> </div>  
+                    <div className='app-header-contents-nav-items'>Pricing</div>
+                    <div className='app-header-contents-nav-items'>Contact sales</div>
+                    <div className='app-header-contents-nav-items'>Log in</div> 
+                    <button className='btn-started'>Get Started</button>
+                </div>
+            </section>
         </header>
     )
 }
