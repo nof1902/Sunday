4ed2bb2c68755699e28341591ae3cd922b0a994a
OrenMela <120256051+OrenMela@users.noreply.github.com>
2024-01-23 22:19:11 +0200

    Changes to sidenav

--- src/cmps/SideNav.jsx
+++ src/cmps/SideNav.jsx
@@ -1,16 +1,32 @@
 
 // import routes from '../routes'
 import { NavLink, useParams } from "react-router-dom"
+import { svgService } from "../svg.service"
 
 export function SideNav({ boards }) {
+
+    const homeIcon = svgService.getSvg('home')
+    const homeIconUrl = `data:image/svg+xml,${encodeURIComponent(homeIcon)}`
+
+    const boardIcon = svgService.getSvg('clipboard')
+    const boardIconUrl = `data:image/svg+xml,${encodeURIComponent(boardIcon)}`
 
     // const params = useParams()
     return (
         <nav className="side-navigation">
             {/* {routes.map(route => <NavLink key={route.path} to={route.path}>{route.label}</NavLink>)} */}
+            <div className="sidenav-home">
+                 <img src={homeIconUrl} />
+                 <NavLink to={`/boards`}>Home</NavLink>
+            </div>
             
-            <NavLink to={`/boards`}>Home</NavLink>
-            {boards.map(board => <NavLink key={board._id} to={`/boards/${board._id}`}>{board.title}</NavLink>)}
+                
+                {boards.map(board =>  (
+                        <div className="sidenav-home">
+                        <img src={boardIconUrl} />
+                        <NavLink key={board._id} to={`/boards/${board._id}`}>{board.title}    </NavLink>
+                        </div>
+                        ))}
         </nav>
     )
 }
