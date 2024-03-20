7f51f0146f15a185cf97735df6ebe3b669368228
nof1902 <nof1902@gmail.com>
2024-03-20 16:20:35 +0200

    change format of date at timeline copm

--- src/cmps/summry-dynamic-cmps/TimeLineSummery.jsx
+++ src/cmps/summry-dynamic-cmps/TimeLineSummery.jsx
@@ -1,8 +1,10 @@
 
-import { format } from "date-fns";
+import { format, formatDistance } from "date-fns";
+import { useState } from "react";
 
 export function TimeLineSummery({ group }) {
 
+  const [isHovered, setIsHovered] = useState(false);
   let minDate = new Date()
   let maxDate = new Date(0)
 
@@ -20,22 +22,32 @@ export function TimeLineSummery({ group }) {
   });
 
   const hasTimelineData = group.tasks.length > 0
-
+  
   function setBackgroundColor() {
     return hasTimelineData ? `#333333` : "rgb(196, 196, 196)"
   }
 
-  const timelineSummary = hasTimelineData
-  ? `${format(minDate, "LLL d")} - ${format(maxDate, "LLL d")}`
-  : '-';
+  function onHover() {
+    if (!hasTimelineData) return "-"; // Return a placeholder if there's no selected timeline
+    if (isHovered) {
+      return formatDistance(minDate, maxDate);
+    } else {
+      if (format(minDate, "LLL") !== format(maxDate, "LLL")) {
+        return `${format(minDate, "LLL d")}-${format(maxDate, "LLL d")}`;
+      } else {
+        return `${format(minDate, "d")}-${format(maxDate, "d LLL")}`;
+      }
+    }
+  }
 
     return (
       <section className="timeline">
         <button
           style={{ background: setBackgroundColor() }}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
           className="time-line-btn"
-        >
-          <span className="time-for-task summery">{timelineSummary}</span>
+        ><span className="time-for-task summery">{onHover()}</span>
         </button>
       </section>
     );
