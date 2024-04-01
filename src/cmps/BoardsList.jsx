509dec8bc61da8f297d8a7baf858a7b32e78a415
nof1902 <nof1902@gmail.com>
2024-04-01 17:24:23 +0300

    board list header

--- src/cmps/BoardsList.jsx
+++ src/cmps/BoardsList.jsx
@@ -5,10 +5,19 @@ import { useOutletContext } from "react-router-dom";
 export function BoardsList({boards}) {
   
   return (
-      <ul className="boards-list">
-          {boards.map(board => ( <BoardPreview key={board._id} board={board}/>
-          ))}
-      </ul>
+    <section className="boards-list">
+      <section className="boards-list-header">
+        <h4>Welcome Sapir!</h4>
+        <h4>Quickly access your boards</h4>
+        <img src="./Images/header-background-v2"></img>
+      </section>
+      <section className="board-of-boards">
+        <ul className="boards">
+            {boards.map(board => ( <BoardPreview key={board._id} board={board}/>
+            ))}
+        </ul>
+      </section>
+    </section>
   )
 }
 
